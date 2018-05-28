38a853007bc47e54d4e9cf293367f05e6716243d
Gil Amran <gil@orbs.com>
2018-05-28 14:55:31 +0300

    Added PBFTStorage assumtions on the docs

--- src/storage/PBFTStorage.ts
+++ src/storage/PBFTStorage.ts
@@ -1,3 +1,8 @@
+//
+// This API stores messages that the PBFT received.
+// The only assumption is that a message will be stored only once per blockHash & senderId
+// For example: If a storePrepare is called twice with the same blockHash and sender id, it will be stored once.
+//
 export interface PBFTStorage {
     storePrePrepare(blockHash: string): void;
     hasPrePrepare(blockHash: string): boolean;
