d2cbd6de2796c57472e0514045de7a5d7e81fad9
Gil Amran <gil@orbs.com>
2018-07-04 16:39:57 +0300

    protecting against PrePrepare not from the leader

--- test/PBFTTerm.spec.ts
+++ test/PBFTTerm.spec.ts
@@ -4,8 +4,10 @@ import * as chai from "chai";
 import { expect } from "chai";
 import * as sinon from "sinon";
 import * as sinonChai from "sinon-chai";
+import { Block } from "../src/Block";
 import { Config } from "../src/Config";
 import { PBFTTerm } from "../src/PBFTTerm";
+import { aBlock, theGenesisBlock } from "./builders/BlockBuilder";
 import { aConfig } from "./builders/ConfigBuilder";
 import { ElectionTriggerMock } from "./electionTrigger/ElectionTriggerMock";
 import { blockMatcher } from "./matchers/blockMatcher";
@@ -29,35 +31,73 @@ describe("PBFTTerm", () => {
     it("onNewView should not accept views from the past", async () => {
         const { config, triggerElection } = init();
 
-        const pbftTerm: PBFTTerm = new PBFTTerm(config, 10, () => { });
+        const pbftTerm: PBFTTerm = new PBFTTerm(config, 0, () => { });
         expect(pbftTerm.getView()).to.equal(0);
         triggerElection();
         expect(pbftTerm.getView()).to.equal(1);
-        pbftTerm.onReceiveNewView("node0", { term: 10, view: 0, PP: undefined });
+        pbftTerm.onReceiveNewView("node0", { term: 0, view: 0, PP: undefined });
         expect(pbftTerm.getView()).to.equal(1);
     });
 
     it("onViewChange should not accept views from the past", async () => {
         const { config, triggerElection } = init();
 
-        const pbftTerm: PBFTTerm = new PBFTTerm(config, 10, () => { });
+        const pbftTerm: PBFTTerm = new PBFTTerm(config, 0, () => { });
         expect(pbftTerm.getView()).to.equal(0);
         triggerElection();
         expect(pbftTerm.getView()).to.equal(1);
 
         const spy = sinon.spy(config.pbftStorage, "storeViewChange");
         // current view (1) => valid
-        pbftTerm.onReceiveViewChange("node0", { term: 10, newView: 1 });
+        pbftTerm.onReceiveViewChange("node0", { term: 0, newView: 1 });
         expect(spy).to.have.been.called;
 
         // view from the future (2) => valid
         spy.resetHistory();
-        pbftTerm.onReceiveViewChange("node0", { term: 10, newView: 2 });
+        pbftTerm.onReceiveViewChange("node0", { term: 0, newView: 2 });
         expect(spy).to.have.been.called;
 
         // view from the past (0) => invalid, should be ignored
         spy.resetHistory();
-        pbftTerm.onReceiveViewChange("node0", { term: 10, newView: 0 });
+        pbftTerm.onReceiveViewChange("node0", { term: 0, newView: 0 });
+        expect(spy).to.not.have.been.called;
+    });
+
+    it("onReceivePrepare should not accept messages from the leader", async () => {
+        const { config, triggerElection } = init();
+
+        const pbftTerm: PBFTTerm = new PBFTTerm(config, 0, () => { });
+        const leaderId = config.network.getNodeIdBySeed(0);
+        const noneLeaderId = config.network.getNodeIdBySeed(1);
+
+        const spy = sinon.spy(config.pbftStorage, "storePrepare");
+        // not from the leader => ok
+        pbftTerm.onReceivePrepare(noneLeaderId, { term: 0, view: 0, blockHash: "" });
+        expect(spy).to.have.been.called;
+
+        // from the leader => ignore
+        spy.resetHistory();
+        pbftTerm.onReceivePrepare(leaderId, { term: 0, view: 0, blockHash: "" });
+        expect(spy).to.not.have.been.called;
+    });
+
+    it("onReceivePrePrepare should not accept messages not from the leader", async () => {
+        const { config, triggerElection } = init();
+
+        const pbftTerm: PBFTTerm = new PBFTTerm(config, 0, () => { });
+        const leaderId = config.network.getNodeIdBySeed(0);
+        const noneLeaderId = config.network.getNodeIdBySeed(1);
+
+        const block: Block = aBlock(theGenesisBlock);
+
+        const spy = sinon.spy(config.blocksValidator, "validateBlock");
+        // from the leader => ok
+        pbftTerm.onReceivePrePrepare(leaderId, { term: 0, view: 0, block });
+        expect(spy).to.have.been.called;
+
+        // not from the leader => ignore
+        spy.resetHistory();
+        pbftTerm.onReceivePrePrepare(noneLeaderId, { term: 0, view: 0, block });
         expect(spy).to.not.have.been.called;
     });
 });
