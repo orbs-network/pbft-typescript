50a2562e7668dfe4639f2b2a57e6274eee339599
Gil Amran <gil@orbs.com>
2018-07-09 17:01:08 +0300

    expose TimerBasedElectionTrigger

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export { BlocksProvider } from "./blocksProvider/BlocksProvider";
 export { BlockStorage } from "./blockStorage/BlockStorage";
 export { BlocksValidator } from "./blocksValidator/BlocksValidator";
 export { Config } from "./Config";
+export { TimerBasedElectionTrigger } from "./electionTrigger/TimerBasedElectionTrigger";
 export { ElectionTrigger, ElectionTriggerFactory } from "./electionTrigger/ElectionTrigger";
 export { Gossip, PreprepareCallback, PrepareCallback, CommitCallback, NewViewCallback, ViewChangeCallback } from "./gossip/Gossip";
 export { Logger, LogTypes } from "./logger/Logger";
