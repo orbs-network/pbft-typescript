d0743552afd133ef8a4c04e1c45659aa1326c0b2
Gil Amran <gil@orbs.com>
2018-07-11 18:11:45 +0300

    Changed the interface of Committee

--- src/committee/Committee.ts
+++ src/committee/Committee.ts
@@ -1,5 +1,6 @@
 export interface Committee {
     getCommitteeMembersPKs(height: number, seed: number): string[];
-    sendToMembers(pks: string[]): void;
-    onMessageReceived(fromAddress: string, messageType: string, message: any): void;
+    sendToMembers(pks: string[], messageType: string, message: any): void;
+    subscribeToMessages(cb: (messageType: string, payload: any) => void): number;
+    unsubscribeFromMessages(subscription: number): void;
 }
